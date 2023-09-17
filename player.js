0bbbd8d62d92f4d299487ac0e2ee504713369042
Sirajulislambg <sirajulislambg256@gmail.com>
2023-09-17 15:39:00 +0600

    second_time

--- player.js
+++ player.js
@@ -6,6 +6,11 @@
     const perPlayer = document.getElementById(elementId).value;
     const playerValue = parseFloat(perPlayer);
 
+    // if(isNaN(playerValue)){
+    //   alert('Please enter a Number!!');
+    //   return;
+    // }
+
     return playerValue;
 
    }
@@ -58,19 +63,32 @@
 
    
    //  function calculate() {
-        const counter = (document.getElementById("counter").innerText =
-          PlayerNameArray.length);
+        const counter = (document.getElementById("counter").innerText =PlayerNameArray.length);
         const parCounter = parseFloat(counter);
 
     document.getElementById("cal").addEventListener('click',function(){
         // const totalPer = document.getElementById("total-per-player");
         // const perPlayer = document.getElementById("per-player").value;
         // const playerValue = parseFloat(perPlayer);
-       const value= inputCalculate("per-player");
 
-       
-       const totalCost = parCounter * value;
-       setValue("total-per-player",totalCost);
+        // if(isNaN()!==0){
+
+           const value = inputCalculate("per-player");
+
+          
+
+           const totalCost = parCounter * value;
+
+          //  if(isNaN(totalCost)){
+          //   alert("Please enter a correct number");
+          //   return;
+          //  }
+           setValue("total-per-player", totalCost);
+        // }
+        // else{
+        //   console.log("please enter something");
+        // }
+      
       
 
         // totalPer.innerText = totalCost;
@@ -84,19 +102,33 @@
 
 
     document.getElementById("final").addEventListener("click", function() {
-         const mangerCost= inputCalculate("manger-cost");
 
-         const coachingCost = inputCalculate("coch-cost");
+      // if(isNaN()!==0){
 
-         const value = inputCalculate("per-player");
-        const previousTotal =value*parCounter;
+        const mangerCost = inputCalculate("manger-cost");
 
-         const CostOfTotal=mangerCost+coachingCost+previousTotal;
+        const coachingCost = inputCalculate("coch-cost");
+
+        const value = inputCalculate("per-player");
+
+        // if (isNaN(value)) {
+        //   alert("Please enter a Number!!");
+        //   return;
+        // }
+        const previousTotal = value * parCounter;
+
+        const CostOfTotal = mangerCost + coachingCost + previousTotal;
         //  console.log(mangerCost);
         //  console.log(CostOfTotal);
         //  console.log(coachingCost);
 
-          const finalCoast = setValue("final-cost-text",CostOfTotal);
+        const finalCoast = setValue("final-cost-text", CostOfTotal);
+
+      // }
+      // else{
+      //   console.log("please enter a value");
+      // }
+         
 
      
     });
@@ -108,17 +140,29 @@
 function selectPlayer(player) {
 
     const PlayerName = player.parentNode.parentNode.children[0].innerText;
+    // player.setAttribute("disabled", "true");
+    // console.log(player.parentNode.parentNode.children[0]);
+    // console.log(player);
+    
   
 
     const playerObj={
         playerName:PlayerName,
     }
 
+    if(PlayerNameArray.length<5){
 
-    PlayerNameArray.push(playerObj);
+      PlayerNameArray.push(playerObj);
+      player.setAttribute("disabled", "true");
+
+    }
+    else{
+      alert("you can not add more  than 5 player !!!!");
+    }
+    // PlayerNameArray.push(playerObj);
 
     const counter = document.getElementById("counter").innerText=PlayerNameArray.length;
-    console.log(PlayerNameArray);
+    // console.log(PlayerNameArray);
 
     displayOfPlayer(PlayerNameArray);
 
